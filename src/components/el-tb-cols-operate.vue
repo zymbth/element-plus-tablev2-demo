ce76cfe306be73929b5f4a687de39504630a836b
ymzhao <ymzhaobth@gmail.com>
2024-06-02 21:18:06 +0800

    更新 .deep 语法

--- src/components/el-tb-cols-operate.vue
+++ src/components/el-tb-cols-operate.vue
@@ -4,11 +4,11 @@ import { ref, toRefs, watch } from 'vue'
 /**
  * el-table columns 显示/隐藏功能组件
  * 只操作 column 的 hidden 属性
- * 
+ *
  * @prop {Array} columnData 表格columns数据
  */
- const props = defineProps({
-  columnData: { type: Array, default: [] }
+const props = defineProps({
+  columnData: { type: Array, default: [] },
 })
 
 const { columnData } = toRefs(props)
@@ -21,17 +21,25 @@ function handleBeforeEnter() {
 }
 // 全选状态
 const checkAll = ref(false)
-watch(shownCols, (newVal) => {
-  checkAll.value = newVal.length === columnData.value.length
-}, { deep: true })
+watch(
+  shownCols,
+  newVal => {
+    checkAll.value = newVal.length === columnData.value.length
+  },
+  { deep: true }
+)
 // 中间状态
 const isIndeterminate = ref(false)
-watch([shownCols,checkAll], ([cols,all]) => {
-  isIndeterminate.value = !all && cols.length > 0
-}, { deep: true })
+watch(
+  [shownCols, checkAll],
+  ([cols, all]) => {
+    isIndeterminate.value = !all && cols.length > 0
+  },
+  { deep: true }
+)
 // 事件：全选/取消全选
 function handleCheckAllChange(val) {
-  if(val) shownCols.value = columnData.value.map(c => c.dataKey)
+  if (val) shownCols.value = columnData.value.map(c => c.dataKey)
   else shownCols.value = columnData.value.filter(c => c.alwaysShow).map(c => c.dataKey)
 }
 // 取消选择
@@ -42,7 +50,7 @@ function handleCancelColsOperation() {
 function handleColsOperation() {
   columnData.value.forEach(c => {
     const show = shownCols.value.includes(c.dataKey)
-    if(!c.hidden !== show) c.hidden = !show
+    if (!c.hidden !== show) c.hidden = !show
   })
   optColsRef.value.hide()
 }
@@ -53,9 +61,7 @@ function handleColsOperation() {
     trigger="click"
     placement="bottom"
     :width="200"
-    class="operate-tb-columns"
-    @before-enter="handleBeforeEnter"
-  >
+    @before-enter="handleBeforeEnter">
     <template #reference>
       <el-button style="margin-left: 16px">Show/Hide columns</el-button>
     </template>
@@ -64,23 +70,22 @@ function handleColsOperation() {
         v-model="checkAll"
         :indeterminate="isIndeterminate"
         @change="handleCheckAllChange"
-      >Check all</el-checkbox>
-      <el-divider style="margin:4px 0" />
+        >Check all</el-checkbox
+      >
+      <el-divider style="margin: 4px 0" />
       <el-checkbox-group v-model="shownCols">
         <el-checkbox
           v-for="col in columnData"
           :key="col.dataKey"
           :label="col.dataKey"
           :disabled="!!col.alwaysShow"
-        >{{ col.title }}</el-checkbox>
+          >{{ col.title }}</el-checkbox
+        >
       </el-checkbox-group>
-      <div style="text-align:right">
+      <div style="text-align: right">
         <el-button size="small" @click="handleCancelColsOperation">Cancel</el-button>
         <el-button type="primary" size="small" @click="handleColsOperation">Confirm</el-button>
       </div>
     </div>
   </el-popover>
 </template>
-<style lang="scss" scoped>
-.operate-tb-columns:deep .el-checkbox {display:flex}
-</style>
--- src/views/group-header.vue
+++ src/views/group-header.vue
@@ -3,92 +3,94 @@ import { ref, onMounted, watch } from 'vue'
 import { TableV2SortOrder } from 'element-plus'
 
 onMounted(() => {
-  getData();
-});
+  getData()
+})
 
-const sortByNum = (a, b, prop) => a[prop] - b[prop];
-const sortByStr = (a, b, prop) => a[prop].localeCompare(b[prop], "zh-CN");
+const sortByNum = (a, b, prop) => a[prop] - b[prop]
+const sortByStr = (a, b, prop) => a[prop].localeCompare(b[prop], 'zh-CN')
 
-const originData = ref([]);
-const tableData = ref([]);
+const originData = ref([])
+const tableData = ref([])
 const columns = [
   {
-    key: "no",
-    dataKey: "no",
-    title: "No.",
+    key: 'no',
+    dataKey: 'no',
+    title: 'No.',
     width: 60,
     sortable: true,
-    sortMethod: sortByNum
+    sortMethod: sortByNum,
   },
-  { key: "code", dataKey: "code", title: "code", width: 80 },
-  { key: "name", dataKey: "name", title: "name", width: 80 },
+  { key: 'code', dataKey: 'code', title: 'code', width: 80 },
+  { key: 'name', dataKey: 'name', title: 'name', width: 80 },
   {
-    key: "age",
-    dataKey: "age",
-    title: "Age",
+    key: 'age',
+    dataKey: 'age',
+    title: 'Age',
     width: 60,
     sortable: true,
-    sortMethod: sortByNum
+    sortMethod: sortByNum,
   },
   {
-    key: "gender",
-    dataKey: "gender",
-    title: "gender",
+    key: 'gender',
+    dataKey: 'gender',
+    title: 'gender',
     width: 80,
-    sortable: true
+    sortable: true,
   },
   {
-    key: "city",
-    dataKey: "city",
-    title: "City",
+    key: 'city',
+    dataKey: 'city',
+    title: 'City',
     width: 80,
-    sortable: true
-  }
-];
+    sortable: true,
+  },
+]
 
 // 排序状态，是ref对象变量。key: 排序项的key, order: 排序值(asc/desc)
-const sortState = ref({ key: "no", order: TableV2SortOrder.ASC });
+const sortState = ref({ key: 'no', order: TableV2SortOrder.ASC })
 
 watch(
   [sortState, originData],
   ([newState, newData]) => {
-    if (!newData?.length) return;
-    const { key, order } = newState ?? {};
+    if (!newData?.length) return
+    const { key, order } = newState ?? {}
     if (!key || !order) {
       // 当前无排序，重置 tableData 顺序
-      tableData.value = originData.value;
-      return;
+      tableData.value = originData.value
+      return
     }
-    const currCol = columns.find((c) => c.key === key);
-    if (!currCol) return;
-    const currSortMethod = currCol.sortMethod ?? sortByStr;
+    const currCol = columns.find(c => c.key === key)
+    if (!currCol) return
+    const currSortMethod = currCol.sortMethod ?? sortByStr
     // 进行排序
     tableData.value = originData.value.slice(0).sort((a, b) => {
-      const res = currSortMethod(a, b, currCol.dataKey);
-      return order === TableV2SortOrder.ASC ? res : 0 - res;
-    });
+      const res = currSortMethod(a, b, currCol.dataKey)
+      return order === TableV2SortOrder.ASC ? res : 0 - res
+    })
   },
   { immediate: true }
-);
+)
 // 排序事件处理
 const onSort = ({ key, order }) => {
-  if (!order) order = TableV2SortOrder.ASC;
+  if (!order) order = TableV2SortOrder.ASC
   // 单项排序，sortState 只记录最新排序信息
-  sortState.value = { key, order };
-};
+  sortState.value = { key, order }
+}
 
-const CustomizedHeader  = ({ cells, columns, headerIndex }) => {
+const CustomizedHeader = ({ cells, columns, headerIndex }) => {
   const groupCells = []
-  for(let i = 0, len = columns.length; i < len; i++) {
-    if(i === 3) {
-      const width = cells[i].props.column.width + cells[i+1].props.column.width
-      groupCells.push(<div
-        class="cell-group"
-        style={{ width: `${width}px` }}
-      >
-        <div class="group-title">Group</div>
-        <div class="cells-wrap">{cells[i]}{cells[i+1]}</div>
-      </div>)
+  for (let i = 0, len = columns.length; i < len; i++) {
+    if (i === 3) {
+      const width = cells[i].props.column.width + cells[i + 1].props.column.width
+      groupCells.push(
+        <div class='cell-group' style={{ width: `${width}px` }}>
+          <div class='group-title'>Group</div>
+          <div class='cells-wrap'>
+            {cells[i]}
+            {cells[i + 1]}
+          </div>
+        </div>
+      )
       i++
     } else {
       groupCells.push(cells[i])
@@ -97,14 +99,14 @@ const CustomizedHeader  = ({ cells, columns, headerIndex }) => {
   return groupCells
 }
 
-const getData = (total) => {
-  getTableData(total).then((res) => {
-    originData.value = res ?? [];
-    tableData.value = originData.value;
-  });
-};
-const getTableData = (total) => {
-  if (!total) total = Math.floor(Math.random() * 2000 + 1000);
+const getData = total => {
+  getTableData(total).then(res => {
+    originData.value = res ?? []
+    tableData.value = originData.value
+  })
+}
+const getTableData = total => {
+  if (!total) total = Math.floor(Math.random() * 2000 + 1000)
   return new Promise((resolve, reject) => {
     resolve(
       Array.from({ length: total }).map((_, idx) => {
@@ -113,13 +115,13 @@ const getTableData = (total) => {
           code: Math.floor(Math.random() * 100000).toString(16),
           name: Math.floor(Math.random() * 100000).toString(16),
           age: Math.floor(Math.random() * 30 + 18),
-          gender: Math.random() > 0.5 ? "男" : "女",
-          city: ["北京", "上海", "深圳"][Math.floor(Math.random() * 3)]
-        };
+          gender: Math.random() > 0.5 ? '男' : '女',
+          city: ['北京', '上海', '深圳'][Math.floor(Math.random() * 3)],
+        }
       })
-    );
-  });
-};
+    )
+  })
+}
 </script>
 
 <template>
@@ -131,8 +133,7 @@ const getTableData = (total) => {
     @column-sort="onSort"
     :width="666"
     :height="666"
-    :fixed="true"
-  >
+    :fixed="true">
     <template #header="props">
       <CustomizedHeader v-bind="props" />
     </template>
@@ -142,8 +143,12 @@ const getTableData = (total) => {
   <el-button @click="getData()">刷新表格数据</el-button>
 </template>
 <style lang="scss" scoped>
-:deep .cell-group {
-  .group-title {text-align: center;}
-  .cells-wrap {display: flex;}
+:deep(.cell-group) {
+  .group-title {
+    text-align: center;
+  }
+  .cells-wrap {
+    display: flex;
+  }
 }
 </style>
