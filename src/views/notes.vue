d7d3305eb5ffaf8e102d1de26c22bc4b7e2c6c86
ymzhao <ymzhaobth@gmail.com>
2026-01-16 00:18:25 +0800

    更新demo

--- src/views/notes.vue
+++ src/views/notes.vue
@@ -14,7 +14,7 @@ const code = `<el-table-v2
   <section>
     <h3>Notes:</h3>
     <h4>1. 动态高度行的使用：</h4>
-    <p>使用 <code>estimated-row-height</code> 属性设置预估高度，<code>row-height</code> 属性会失效。</p>
+    <p>使用 <code>estimated-row-height</code> 属性设置预估高度，<code>row-height</code> 属性会失效（高度会“坍塌”下来，需要通过设置行高、单元格边距等方式“撑起”一定的边距）。</p>
     <p>最重要的，<code>row-key</code> 默认为 <code>id</code>，表格数据中没有id属性的话，需指定 <code>row-key</code>，否则，动态高度行无法生效！</p>
     <p>示例：</p>
     <pre>{{ code }}</pre>
